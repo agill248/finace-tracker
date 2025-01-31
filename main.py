0ec2184ab7f68c96ad4342ad5838b494db8de313
agill248 <aveerg367@deltalearns.ca>
2025-01-31 14:37:09 -0800

    Tried making Finace Tracker
    Wrote allowence questions

---
+++ main.py
@@ -0,0 +1,9 @@
+# finace tracker
+# greeting
+print("Welcome to the Finace Tracker! Here you can track all your Finaces for the month!") 
+# ask for monthly allowence and expenses in different categories
+Overall_Allowence = int(input("What was your overall allowence for the month!"))
+Food = int(input("How much have you spent on food?"))
+Entertainment = int(input("How much money have you spent on enterainment?"))
+Clothes = int(input("How much money have you spent on clothes?"))
+Misellaneous = int(input("How much did you spend on things that weren't listed before?"))
