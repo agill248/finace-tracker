8114cc36b7eace03bf26332842461ad7c4c17802
agill248 <aveerg367@deltalearns.ca>
2025-02-20 21:23:59 +0000

    Started the Grocery list with 2 functions

---
+++ grocery.py
@@ -0,0 +1,21 @@
+# This is a grocery store list
+
+
+# Define a function to add an item to the list
+def add_food(grocery_list: list, food: str):
+    grocery_list.append(food)
+    return grocery_list
+
+# Define a function to remove an item from the list
+def remove_food(grocery_list: list, food: str):
+    remove_food.remove(food)
+if not remove_food:
+    print("Please remove something that is in your list.")
+
+# If not, display a message saying the item is not in the list
+
+# Define a function to display the grocery list
+
+# If the list is not empty, print all items with numbers
+
+# If the list is empty, display a message
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# finace tracker
-# greeting
-print("Welcome to the Finace Tracker! Here you can track all your Finaces for the month!") 
-# ask for monthly allowence and expenses in different categories
-Overall_Allowence = int(input("What was your overall allowence for the month!"))
-Food = int(input("How much have you spent on food?"))
-Entertainment = int(input("How much money have you spent on enterainment?"))
-Clothes = int(input("How much money have you spent on clothes?"))
-Misellaneous = int(input("How much did you spend on things that weren't listed before?"))
+# Create an High Score Tracker
+
+# Create function to update the score
+def update_score()
+# Make a list for the score
+ultimate_score = []
+
+# Let the player input diffrent scores
